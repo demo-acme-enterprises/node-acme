1fdb4d4a77aad1f76d56d39cfcccfa0d64180b00
Jeff Andersen <77063352+jeffsnyk@users.noreply.github.com>
2025-09-10 09:29:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const SECRET_API_TOKEN = 'abc1234skdhusdgf9732bwcf';
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
