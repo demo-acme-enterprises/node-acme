b6e8008a93c2cc9ef68a00217f66673d2f8e57e0
Jeff Andersen <jeff.andersen@snyk.io>
2025-10-27 13:39:22 -0300

    Add changes for login

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require('express')
+const helmet = require('helmet')
+const session = require('express-session')
 const app = express()
+app.use(helmet())
 const port = 3000
 
-const SECRET_TOKEN = 'abc12e2o3cenodsnlknewd';
+const auth = (un, pw) => { return true }
+const createToken = (un) => { return 'abc123' }
+
+app.use(session({
+  secret: 'keyboard cat',
+  name: 'my-session',
+  cookie: { path: '/', secure: true }
+}))
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!auth(username, password)) {
+    res.status(400).send("Incorrect credentials");
+    return;
+  }
+
+  // Set the session cookie
+  res.cookie('auth', createToken(username), {
+    domain: '.acme.corp',
+    path: '/',
+    expires: new Date(Date.now() + 60 * 60 * 1000),
+    secure: true
+  });
+
+  res.redirect('/feed')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
