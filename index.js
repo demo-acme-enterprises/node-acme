b995e122075bf2c1673a6ccb6bcc0f1861043481
Jeff Andersen <77063352+jeffsnyk@users.noreply.github.com>
2025-10-28 15:26:16 -0300

    Remove SECRET_TOKEN from index.js
    Removed the SECRET_TOKEN constant from index.js.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const SECRET_TOKEN = 'abc12e2o3cenodsnlknewd';
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
