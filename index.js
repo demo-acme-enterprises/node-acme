84c7a6ce09d74904c9faf995cdeafdf56de9194b
Jeff Andersen <jeff.andersen@snyk.io>
2026-05-11 10:14:26 -0300

    Change startup log message on feat/branch-conflict

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`node-acme server is running at http://localhost:${port}`)
 })
